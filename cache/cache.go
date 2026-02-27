85cadbe80d43efa8423782fd78560e3556dea342
Aryan Sharma <aryan182282@gmail.com>
2026-02-28 03:25:38 +0530

    added lazy eviction logic

--- cache/cache.go
+++ cache/cache.go
@@ -1,94 +1,114 @@
-package cache
+	package cache
 
-import (
-	"aryan-errs/simple-cache/cache/types"
-	"container/heap"
-	"time"
-)
+	import (
+		expiryheap "aryan-errs/simple-cache/cache/expiryHeap"
+		"aryan-errs/simple-cache/cache/types"
+		"container/heap"
+		"time"
+	)
 
-type cache struct {
-	capacity    int
-	KeyMap      map[int]*types.Node
-	head        *types.Node
-	tail        *types.Node
-	ttlDuration time.Duration
-	expiryHeap  heap.Interface
-}
-
-func CreateCache(capacity int) *cache {
-	c := &cache{
-		capacity:    capacity,
-		KeyMap:      make(map[int]*types.Node),
-		head:        &types.Node{},
-		tail:        &types.Node{},
-		ttlDuration: time.Duration(10 * time.Second),
+	type cache struct {
+		capacity    int
+		KeyMap      map[int]*types.Node
+		head        *types.Node
+		tail        *types.Node
+		ttlDuration time.Duration
+		expiryHeap  *expiryheap.ExpiryHeap
 	}
-	c.head.Next = c.tail
-	c.tail.Prev = c.head
-	return c
-}
 
-func (c *cache) remove(currNode *types.Node) {
-	PrevNode := currNode.Prev
-	NextNode := currNode.Next
-
-	PrevNode.Next = NextNode
-	NextNode.Prev = PrevNode
-
-	currNode.Prev = nil
-	currNode.Next = nil
-}
-
-func (c *cache) addToMru(newNode *types.Node) {
-	lastRealNode := c.tail.Prev
-	lastRealNode.Next = newNode
-	newNode.Prev = lastRealNode
-	c.tail.Prev = newNode
-	newNode.Next = c.tail
-}
-
-func (c *cache) evictLru() {
-	lruNode := c.head.Next
-	delete(c.KeyMap, lruNode.Key)
-	c.head.Next = lruNode.Next
-	lruNode.Next.Prev = c.head
-	lruNode.Prev = nil
-	lruNode.Next = nil
-}
-
-func (c *cache) Get(Key int) int {
-	if currNode, ok := c.KeyMap[Key]; ok {
-		c.remove(currNode)
-		if time.Now().After(currNode.ExpiresAt) {
-			delete(c.KeyMap, Key)
-			return -1
+	func CreateCache(capacity int) *cache {
+		c := &cache{
+			capacity:    capacity,
+			KeyMap:      make(map[int]*types.Node),
+			head:        &types.Node{},
+			tail:        &types.Node{},
+			ttlDuration: time.Duration(10 * time.Second),
 		}
-		c.addToMru(currNode)
-		return currNode.Value
+		c.head.Next = c.tail
+		c.tail.Prev = c.head
+		c.expiryHeap = &expiryheap.ExpiryHeap{}
+		heap.Init(c.expiryHeap)
+		return c
 	}
-	return -1
-}
 
-func (c *cache) Put(Key int, val int) bool {
-	if existingNode, ok := c.KeyMap[Key]; ok {
-		c.remove(existingNode)
-		existingNode.Value = val
-		existingNode.ExpiresAt = time.Now().Add(c.ttlDuration)
-		c.addToMru(existingNode)
-		heap.Push(c.expiryHeap, types.ExpiryItem{
-			ExpiresAt: existingNode.ExpiresAt,
-			Node:      existingNode,
+	func (c *cache) remove(currNode *types.Node) {
+		PrevNode := currNode.Prev
+		NextNode := currNode.Next
+
+		PrevNode.Next = NextNode
+		NextNode.Prev = PrevNode
+
+		currNode.Prev = nil
+		currNode.Next = nil
+	}
+
+	func (c *cache) addToMru(newNode *types.Node) {
+		lastRealNode := c.tail.Prev
+		lastRealNode.Next = newNode
+		newNode.Prev = lastRealNode
+		c.tail.Prev = newNode
+		newNode.Next = c.tail
+	}
+
+	func (c *cache) evictLru() {
+		lruNode := c.head.Next
+		delete(c.KeyMap, lruNode.Key)
+		c.remove(lruNode)
+	}
+
+	func (c *cache) evict() {
+		for len(c.KeyMap) > c.capacity && c.expiryHeap.Len() > 0 {
+			expiredItem := c.expiryHeap.Peak()
+			if expiredItem.ExpiresAt.After(time.Now()) {
+				break
+			}
+			heap.Pop(c.expiryHeap)
+			node := expiredItem.Node
+			if node.ExpiresAt.Equal(expiredItem.ExpiresAt) {
+				c.remove(node)
+				delete(c.KeyMap, node.Key)
+			}
+		}
+		if len(c.KeyMap) > c.capacity {
+			c.evictLru()
+		}
+	}
+
+	func (c *cache) Get(Key int) int {
+		if currNode, ok := c.KeyMap[Key]; ok {
+			if time.Now().After(currNode.ExpiresAt) {
+				c.remove(currNode)
+				delete(c.KeyMap, Key)
+				return -1
+			}
+			c.remove(currNode)
+			c.addToMru(currNode)
+			return currNode.Value
+		}
+		return -1
+	}
+
+	func (c *cache) Put(Key int, val int) bool {
+		if existingNode, ok := c.KeyMap[Key]; ok {
+			c.remove(existingNode)
+			existingNode.Value = val
+			existingNode.ExpiresAt = time.Now().Add(c.ttlDuration)
+			c.addToMru(existingNode)
+			heap.Push(c.expiryHeap, &types.ExpiryItem{
+				ExpiresAt: existingNode.ExpiresAt,
+				Node:      existingNode,
+			})
+			return false
+		}
+		newNode := &types.Node{Key: Key, Value: val, ExpiresAt: time.Now().Add(c.ttlDuration)}
+		c.addToMru(newNode)
+		c.KeyMap[Key] = newNode
+		heap.Push(c.expiryHeap, &types.ExpiryItem{
+			ExpiresAt: newNode.ExpiresAt,
+			Node:      newNode,
 		})
-		return false
+		if len(c.KeyMap) > c.capacity {
+			c.evict()
+		}
+		return true
 	}
-	newNode := &types.Node{Key: Key, Value: val, ExpiresAt: time.Now().Add(c.ttlDuration)}
-	c.addToMru(newNode)
-	c.KeyMap[Key] = newNode
-	if c.expiryHeap.Len() > 0 {
-
-	}
-	if len(c.KeyMap) > c.capacity {
-		c.evictLru()
-	}
-	return true
-}
---
+++ cache/expiryHeap/expiryHeap.go
@@ -0,0 +1,35 @@
+package expiryheap
+
+import "aryan-errs/simple-cache/cache/types"
+
+type ExpiryHeap []types.ExpiryItem
+
+func (h ExpiryHeap) Len() int {
+	return len(h)
+}
+
+func (h ExpiryHeap) Less(i int, j int) bool {
+	return h[i].ExpiresAt.Before(h[j].ExpiresAt)
+}
+
+func (h ExpiryHeap) Swap(i int, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h ExpiryHeap) Peak() types.ExpiryItem {
+	n := len(h)
+	return h[n-1]
+}
+
+func (h *ExpiryHeap) Push(x any) {
+	*h = append(*h, x.(types.ExpiryItem))
+}
+
+func (h *ExpiryHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	old[n-1] = types.ExpiryItem{}
+	*h = old[0 : n-1]
+	return x
+}
